01045eb6e23f6f944301109c85c58007afc7b502
Anna Jantzen <annajantzen@Annas-MacBook-Pro.local>
2024-01-11 14:56:07 -0600

    Adding inital code

---
+++ assets/questions.js
@@ -0,0 +1,29 @@
+
+
+var questions = [
+    {
+        question: "Arrays in JavaScript can be used to store ____",
+        options: ["Numbers and strings", "Booleans", "Other arrays", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content in the debugger is ____",
+        options: ["Javascript", "Terminal", "for loops", "console.log"],
+        answers: "console.log"
+    },
+    {
+        question: "Commonly used data types do not include",
+        options: ["Strings", "Alerts", "Booleans", "Numbers"],
+        answers: "Alerts"
+    },
+    {
+        question: "String values must be enclosed inside ____ when being assigned to a variable",
+        options: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        answers: "Quotes"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed in ____",
+        options: ["Square Brackets", "Quotes", "Curly Brackets", "Parenthesis"],
+        answers: "Parenthesis"
+    },
+];
